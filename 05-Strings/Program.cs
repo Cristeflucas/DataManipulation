6b6cac36a200f048f30ea449a002626e62242028
Cristeflucas <cristeflucas@hotmail.com>
2026-04-03 14:24:37 -0300

    Duração e lançamento formatados

--- 05-Strings/Program.cs
+++ 05-Strings/Program.cs
@@ -4,24 +4,24 @@ using var arquivo = new FileStream("musicas.csv", FileMode.Open, FileAccess.Read
 using var stream = new StreamReader(arquivo);
 
 var musica = ObterMusicas(stream)
+    .Take(20);
+
+ExibirMusicasEmTabela(musica);
+
+void AlterandoOTitulo(StreamReader stream)
+{
+    var musica = ObterMusicas(stream)
     .Where(m => m.Titulo.StartsWith('T'))
     .OrderBy(m => m.Lancamento)
     .FirstOrDefault();
-if (musica is not null)
-{
-    Console.WriteLine($"Titulo da Musica: {musica.Titulo}");
-    musica.Titulo = musica.Titulo.Replace("The ", ""); // Strings são imutáveis, então isso não altera o titulo da musica
-    Console.WriteLine($"Titulo da Musica: {musica.Titulo}");
+    if (musica is not null)
+    {
+        Console.WriteLine($"Titulo da Musica: {musica.Titulo}");
+        musica.Titulo = musica.Titulo.Replace("The ", ""); // Strings são imutáveis, então isso não altera o titulo da musica
+        Console.WriteLine($"Titulo da Musica: {musica.Titulo}");
+    }
+
 }
-
-
-
-
-//ExibirMusicas(musicasComT);
-
-
-
-
 void ValidandoSenha()
 {
     var senha = "Ml#1234567";
@@ -40,17 +40,38 @@ void ValidandoSenha()
     {
         Console.WriteLine("Senha Forte!");
     }
-    void ExibirMusicas(IEnumerable<Musica> musicas)
+}
+
+void ExibirMusicas(IEnumerable<Musica> musicas)
+{
+    var titulo = "\nMusicas do arquivo";
+    Console.WriteLine(titulo);
+    foreach (var musica in musicas)
     {
-        var titulo = "\nMusicas do arquivo";
-        Console.WriteLine(titulo);
-        foreach (var musica in musicas)
-        {
-            var linha = $"\t- {musica.Titulo} ({musica.Artista}) - {musica.Duracao}s [{musica.Lancamento}]";
-            Console.WriteLine(linha);
-        }
+        var linha = $"\t- {musica.Titulo} ({musica.Artista}) - {musica.Duracao}s [{musica.Lancamento}]";
+        Console.WriteLine(linha);
     }
 }
+
+void ExibirMusicasEmTabela(IEnumerable<Musica> musicas)
+{
+    var titulo = "\nMusicas do arquivo";
+
+    var colunaTitulo = "Titulo".PadRight(40);
+    var colunaArtista = "Artista".PadRight(30);
+    var colunaDuracao = "Duracao".PadRight(10);
+    var colunaLancamento = "Lançanda Em".PadRight(15);
+    Console.WriteLine($"{colunaTitulo}{colunaArtista}{colunaDuracao}{colunaLancamento}");
+    var borda = "".PadRight(100, '=');
+    Console.WriteLine(borda);
+
+    foreach (var musica in musicas)
+    {
+        var linha = $"{musica.Titulo,-40}{musica.Artista,-30}{musica.Duracao/60.0,-10:F2} {musica.Lancamento,-15:dd/MM/yyyy}";
+        Console.WriteLine(linha);
+    }
+}
+
 
 IEnumerable<Musica> ObterMusicas(StreamReader stream)
 {
@@ -78,4 +99,9 @@ class Musica
     public int Duracao { get; set; }
     public IEnumerable<string> Generos { get; set; }
     public DateTime Lancamento { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Titulo} ({Artista}) - {Duracao}s [{Lancamento:dd/MM/yyyy}]";
+    }
 }
